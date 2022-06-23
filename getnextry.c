911acee3d5d7f6de88f7013c8ade5299a2222f31
BakarSemper <daca.cacho@gmail.com>
2022-06-23 15:50:49 +0100

    Update getnextry.c

--- getnextry.c
+++ getnextry.c
@@ -1,7 +1,18 @@
-#include "get_next_line.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
 
 int	checkchr(char *str, char c)
 {
@@ -17,17 +28,26 @@ int	checkchr(char *str, char c)
 	return (-1);
 }
 
-char	*set_tmp(char *buf, char *tmp, int len)
+char	*set_part(char *s1, char *s2, int len)
 {
-	int	tc;
-	int	bc;
+	size_t	i;
+	size_t	j;
+	char	*join;
 
-	tc = 0;
-	bc = 0;
-	while (buf[bc])
-		tmp[tc + bc] = buf[bc++];
-	tmp[tc + bc] = '\0';
-	return (tmp);
+	if (!s1 || !s2)
+		return (NULL);
+	join = malloc((ft_strlen(s1) + (ft_strlen(s2) + len) + 1) * sizeof(char));
+	if (!join)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[i])
+		join[j++] = s1[i++];
+	i = 0;
+	while (s2[i] || s2[i] == 10)
+		join[j++] = s2[i++];
+	join[j] = '\0';
+	return (join);
 }
 
 char	*get_next_line(int fd)
@@ -37,14 +57,24 @@ char	*get_next_line(int fd)
 	int			nbytes;
 
 	nbytes = 0;
-	tmp = NULL;
 	if (fd < 0 || BUFFER_SIZE < 0 || fd > 1024)
 		return (tmp);
 	while (1)
 	{
+		if (buf[0])
+			set_part(tmp, buf, checkchr(buf, 10));
+		if (checkchr(tmp, 10) != -1)
+			break ;
 		nbytes = read(fd, buf, BUFFER_SIZE);
-		if (nbytes != 0)
-			set_tmp(buf, tmp, checkchr(buf, 10));
 	}
 	return (tmp);
+}
+
+int	main(void)
+{
+	int	fd = open("text1.txt", O_RDONLY);
+	char	*str = get_next_line(fd);
+
+	printf("%s", str);
+	return (0);
 }
